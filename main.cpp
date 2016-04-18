43b8087a0068486028ef06ddacea6a87da976084
heavenphenix <david.t.lam@ucdenver.edu>
2016-04-17 23:50:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+//
+// by David Lam
+// 4.17.16
+// 'main.cpp'
+//
+
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <vector>
 
@@ -11,24 +17,23 @@ using std::endl;
 
 using namespace Testing;
 
-int main() {
+int main()
+{
 
     const int NumIters = 3;
 
     cout << endl << "Testing PA5!!" << endl << endl;
 
     cout << "NOTE:  If you see any memory errors, you MUST fix them!" << endl;
-    cout << "       Tests intentionally invoke destructors after they complete,"
-    << endl;
-    cout << "       so if you see a seg-fault after a passed test, it is"
-    << endl;
+    cout << "       Tests intentionally invoke destructors after they complete," << endl;
+    cout << "       so if you see a seg-fault after a passed test, it is" << endl;
     cout << "       probably a bug in your destructor." << endl;
 
     cout << endl;
 
     ErrorContext ec(cout);
 
-    // piece tests
+    // test piece
     test_piece_smoketest(ec);
     test_piece_print(ec, NumIters);
     test_piece_aging(ec, NumIters);
@@ -36,13 +41,13 @@ int main() {
     test_piece_turntaking(ec, NumIters);
     test_piece_interaction(ec, NumIters);
 
-    // surroundings tests
+   // test surrounding
     test_surroundings_smoketest(ec);
 
-    // action tests
+   // test action
     test_action_smoketest(ec);
 
-    // game tests
+  // test game
     test_game_smoketest(ec);
     test_game_populate(ec, NumIters);
     test_game_getpiece(ec, NumIters);
